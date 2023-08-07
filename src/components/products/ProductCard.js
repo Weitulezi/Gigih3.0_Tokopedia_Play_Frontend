78dd1638da2efd1670807df74683dabd1005f16d
Dixon Vande <vandenemo@yahoo.com>
2023-08-08 02:01:56 +0700

    feat: add video details page. Add product list's sidebar and youtube video embed

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,31 +1,34 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const ProductCard = () => {
     return (
-        <div
-            className="border-[2px] border-black-40"
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                rowGap: "6px",
-            }}
-        >
-            <div className="flex justify-center">
-                <img
-                    width="100%"
-                    height="auto"
-                    className="object-cover"
-                    src="/assets/yt-thumbnail.jpg"
-                    alt=""
-                />
+        <Link to="/">
+            <div
+                className="border-[2px] border-black-40"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: "6px",
+                }}
+            >
+                <div className="flex justify-center">
+                    <img
+                        width="100%"
+                        height="auto"
+                        className="object-cover"
+                        src="/assets/yt-thumbnail.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="mx-[10px]">
+                    <h3 className="text-[18px]">Product Name</h3>
+                </div>
+                <div className="mx-[10px] mb-[6px]">
+                    <h5 className="text-[14px]">Rp. 230000</h5>
+                </div>
             </div>
-            <div className="mx-[10px]">
-                <h3 className="text-[18px]">Product Name</h3>
-            </div>
-            <div className="mx-[10px] mb-[6px]">
-                <h5 className="text-[14px]">Rp. 230000</h5>
-            </div>
-        </div>
+        </Link>
     )
 }
 
