234ee9ed61eb4d2a37e59dd3e2c591d042a7afac
Dixon Vande <vandenemo@yahoo.com>
2023-08-08 02:13:46 +0700

    style: update product card styling

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,34 +1,47 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { GiPriceTag } from "react-icons/gi"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 
 const ProductCard = () => {
     return (
-        <Link to="/">
-            <div
-                className="border-[2px] border-black-40"
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    rowGap: "6px",
-                }}
-            >
-                <div className="flex justify-center">
-                    <img
-                        width="100%"
-                        height="auto"
-                        className="object-cover"
-                        src="/assets/yt-thumbnail.jpg"
-                        alt=""
-                    />
-                </div>
-                <div className="mx-[10px]">
-                    <h3 className="text-[18px]">Product Name</h3>
-                </div>
-                <div className="mx-[10px] mb-[6px]">
-                    <h5 className="text-[14px]">Rp. 230000</h5>
-                </div>
+        <div
+            className="border-[2px] border-black-40"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                rowGap: "16px",
+            }}
+        >
+            <div className="flex justify-center">
+                <img
+                    width="100%"
+                    height="auto"
+                    className="object-cover"
+                    src="/assets/yt-thumbnail.jpg"
+                    alt=""
+                />
             </div>
-        </Link>
+            <div className="mx-[14px]">
+                <h3 className="text-[18px]">Lorem ipsum dolor sit.</h3>
+            </div>
+            <div className="flex justify-between items-center  mx-[14px] mb-[14px]">
+                <div className="flex items-center gap-x-[8px]">
+                    <span>
+                        <GiPriceTag />
+                    </span>
+                    <span>Rp. 300000</span>
+                </div>
+                <Link to="/">
+                    <div className="flex items-center gap-x-[4px]">
+                        <span>Add to</span>
+                        <span className="text-[22px]">
+                            <AiOutlineShoppingCart />
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
